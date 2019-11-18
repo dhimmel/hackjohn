96ca43a5add0391540596a23cbdddbea64e92d30
Daniel Himmelstein <daniel.himmelstein@gmail.com>
2019-11-18 17:57:24 -0500

    style: blacken python code

--- hackjohn.py
+++ hackjohn.py
@@ -24,27 +24,27 @@ spaces = 2
 
 # Comment out trailheads you'd like to start from
 exclude = [
-#     'Happy Isles->Little Yosemite Valley',
-#     'Happy Isles->Sunrise/Merced Lake (pass through)',
-    'Glacier Point->Little Yosemite Valley',
-    'Sunrise Lakes',
-    'Lyell Canyon',
+    #     'Happy Isles->Little Yosemite Valley',
+    #     'Happy Isles->Sunrise/Merced Lake (pass through)',
+    "Glacier Point->Little Yosemite Valley",
+    "Sunrise Lakes",
+    "Lyell Canyon",
 ]
 
 # Dates you'd like to start on (inclusive of end date)
-dates = pandas.date_range(start='2019-08-30', end='2019-10-05', freq='D')
+dates = pandas.date_range(start="2019-08-30", end="2019-10-05", freq="D")
 dates
 
 # Write output to this file. If the generated output is identical to
 # the existing output at this path, suppress notification. To disable
 # writing any files, set output_path=None as shown below.
-output_path = pathlib.Path('__file__').parent.joinpath('hackjohn-output.txt')
+output_path = pathlib.Path("__file__").parent.joinpath("hackjohn-output.txt")
 # output_path = None  # None disables writing to a file
 
 # If the Report Date is before this day, suppress Telegram notification.
 # You probably do not need to change this setting unless you have disabled
 # output_path
-min_report_date = '2019-01-01'
+min_report_date = "2019-01-01"
 
 
 def get_trailhead_df():
@@ -56,26 +56,25 @@ def get_trailhead_df():
     if pandas_version[:2] == (0, 23):
         # read_html malfunctions in pandas v0.23
         # https://github.com/pandas-dev/pandas/issues/22135
-        raise ImportError('pandas v0.23 is not supported due to https://git.io/fp9Zn')
+        raise ImportError("pandas v0.23 is not supported due to https://git.io/fp9Zn")
 
-    url = 'https://www.nps.gov/yose/planyourvisit/fulltrailheads.htm'
+    url = "https://www.nps.gov/yose/planyourvisit/fulltrailheads.htm"
     response = requests.get(url)
     response.raise_for_status()
 
-    wide_df, = pandas.read_html(
+    (wide_df,) = pandas.read_html(
         response.text,
         header=1,
-        attrs = {'id': 'cs_idLayout2'},
-        flavor='html5lib',
-        parse_dates=['Date'],
+        attrs={"id": "cs_idLayout2"},
+        flavor="html5lib",
+        parse_dates=["Date"],
     )
     wide_df = wide_df.iloc[:, :6]
 
     trailhead_df = (
-        wide_df
-        .melt(id_vars='Date', var_name='Trailhead', value_name='Spaces')
+        wide_df.melt(id_vars="Date", var_name="Trailhead", value_name="Spaces")
         .dropna()
-        .sort_values(by=['Date'], kind='mergesort')
+        .sort_values(by=["Date"], kind="mergesort")
     )
     trailhead_df.Spaces = trailhead_df.Spaces.astype(int)
     assert len(trailhead_df) > 0
@@ -88,25 +87,27 @@ trailhead_df.head(2)
 
 # Extract report date. https://github.com/dhimmel/hackjohn/issues/1
 try:
-    match = re.search(r'Report Date: ([0-9/]+)', yose_response.text)
+    match = re.search(r"Report Date: ([0-9/]+)", yose_response.text)
     report_date = match.group(1)
     report_date = pandas.to_datetime(report_date, dayfirst=False)
 except Exception:
-    report_date = yose_response.headers['Date']
+    report_date = yose_response.headers["Date"]
     report_date = pandas.to_datetime(report_date, utc=True)
 report_date = report_date.date().isoformat()
 
-space_df = trailhead_df.query("Date in @dates and Spaces >= @spaces and Trailhead not in @exclude")
+space_df = trailhead_df.query(
+    "Date in @dates and Spaces >= @spaces and Trailhead not in @exclude"
+)
 space_df
 
-space_str = 'NO VACANCY' if space_df.empty else space_df.to_string(index=False)
-text = f'''Spaces available as of {report_date}:
+space_str = "NO VACANCY" if space_df.empty else space_df.to_string(index=False)
+text = f"""Spaces available as of {report_date}:
 
 {space_str}
 
 According to {yose_response.url}
 Yosemite Reservations: 209-372-0740 (Monday–Friday 9:00am–4:30pm)
-'''
+"""
 print(text)
 # Detect if output_path has changed. If so, rewrite output.
 output_has_changed = True
@@ -117,7 +118,7 @@ if output_path:
         output_has_changed = text != previous_text
     if output_has_changed:
         output_path.write_text(text)
-print(f'output has changed: {output_has_changed}')
+print(f"output has changed: {output_has_changed}")
 
 # determine whether to notify
 notify = not space_df.empty and output_has_changed and min_report_date <= report_date
@@ -130,26 +131,26 @@ enable_middleman = False
 
 # Get token from messaging /start to @MiddleManBot on Telegram
 # https://telegram.me/MiddleManBot
-token = 'replace-with-private-telegram-middlemanbot-token'
+token = "replace-with-private-telegram-middlemanbot-token"
 
-hostname = 'https://middleman.ferdinand-muetsch.de'
-mmb_url = hostname + '/api/messages'
+hostname = "https://middleman.ferdinand-muetsch.de"
+mmb_url = hostname + "/api/messages"
 payload = {
-    'recipient_token': token,
-    'text': text,
-    'origin': 'hackjohn',
+    "recipient_token": token,
+    "text": text,
+    "origin": "hackjohn",
 }
 if notify and enable_middleman:
     mmb_response = requests.post(mmb_url, json=payload)
-    print('middleman status code', mmb_response.status_code)
+    print("middleman status code", mmb_response.status_code)
     print(mmb_response.text)
 
 ## Notifications using IFTTT
 
 # Set enable_ifttt to True and personalize ifttt_key to receive IFTTT notifications
 enable_ifttt = False
-event_name = 'hackjohn'
-ifttt_key = 'replace-with-ifttt-key'
+event_name = "hackjohn"
+ifttt_key = "replace-with-ifttt-key"
 
 """
 ## IFTTT Setup Instructions
@@ -166,14 +167,14 @@ Go to https://ifttt.com/maker_webhooks and click on documentation,
 copy & paste your key into the `ifttt_key` variable above.
 """
 
-ifttt_hostname = 'https://maker.ifttt.com'
-ifttt_url = ifttt_hostname + '/trigger/' + event_name + '/with/key/' + ifttt_key
+ifttt_hostname = "https://maker.ifttt.com"
+ifttt_url = ifttt_hostname + "/trigger/" + event_name + "/with/key/" + ifttt_key
 
 if notify and enable_ifttt:
     report = {
         "value1": text,
-        "value2": '209-372-0740',
+        "value2": "209-372-0740",
     }
     response = requests.post(ifttt_url, data=report)
-    print('ifttt status code', response.status_code)
+    print("ifttt status code", response.status_code)
     print(response.text)
